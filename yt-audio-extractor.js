f4f8b03943835ca252049437f0f5b6797804e29a
Alex <alexrichards92@me.com>
2016-07-01 14:22:38 -0700

    swapped out child process for node-youtube-dl

--- yt-audio-extractor.js
+++ yt-audio-extractor.js
@@ -1,4 +1,4 @@
-var child = require('child_process')
+var ytdl = require('youtube-dl');
 
 /* This module will check if the given link is a valid YouTube link. If so,
  * then it saves the title of the video into a variable and then downloads the
@@ -6,83 +6,65 @@ var child = require('child_process')
  * object to be pushed into the queue of the booth in question. */
 module.exports = {
   getName: function (obj, callback) {
-    var name = child.spawn('youtube-dl', ['--get-title', obj.link]);
     var nameString = "";
-    name.stdout.on('data', function (data) {
-      nameString = data.toString().split("\n")[0].split(".ogg")[0];
-    });
-    name.stderr.on('data', function (data) {
-      console.log("error with name: "+data.toString());
-      callback("", "", false);
-    });
-    name.on('exit', function (code) {
-      if (code == 0) {
-        callback(nameString, "", true);
+    ytdl.getInfo(obj.link, function (err, info) {
+      if (err) {
+        console.log("There was an error getting info from the given link.");
+        callback('', '', false);
       }
+      nameString = info._filename.split('.webm')[0].split('-').slice(0, -1).join('-');
+      callback(nameString, '', true);
     });
   },
 
   download: function (obj, callback) {
-    var name = child.spawn('youtube-dl', ['--get-title', obj.link]);
     var nameString = "";
-    name.stdout.on('data', function (data) {
-      nameString = data.toString().split("\n")[0].split(".ogg")[0];
-    });
-    name.stderr.on('data', function (data) {
-      console.log("error with name: "+data.toString());
-      callback('', 'error');
-    });
-    name.on('exit', function (code) {
-      if (code == 0) {
-        var hasher = require('crypto').createHash('sha1');
-        hasher.update(nameString+Date.now());
-        var hash = hasher.digest('hex');
-        var dl = child.spawn('youtube-dl', [
-            '--no-playlist', '--extract-audio', '--audio-format', 'vorbis', '--audio-quality', '128k',
-            obj.link, '-o', 'public/songs/'+obj.creator+'/'+nameString+hash+'.%(ext)s']);
-        dl.stdout.on('data', function (data) {
-          console.log(data.toString());
-        });
-        dl.stderr.on('data', function (data) {
-          console.log("error with downloading: "+data.tostring());
-          callback('', 'error');
-        });
-        dl.on('exit', function (code) {
-          callback(hash, null);
-        });
+    ytdl.getInfo(obj.link, function (err, info) {
+      if (err) {
+        console.log("There was an error getting info from the given link.");
+        callback('', 'error');
       }
+      nameString = info._filename.split('.webm')[0].split('-').slice(0, -1).join('-');
+
+      var hasher = require('crypto').createHash('sha1');
+      hasher.update(nameString+Date.now());
+      var hash = hasher.digest('hex');
+
+      ytdl.exec(obj.link,
+        ['--no-playlist', '--extract-audio', '--audio-format', 'vorbis',
+         '--audio-quality', '128k', '-o', 'public/songs/'+obj.creator+
+        '/'+nameString+hash+'.%(ext)s'], {}, function (err, output) {
+        if (err) {
+          console.log("There was an error downloading the song...");
+          callback('', 'error');
+        }
+      });
     });
   },
 
   getNameThenDownload: function (obj, callback) {
-    var name = child.spawn('youtube-dl', ['--get-title', obj.link]);
     var nameString = "";
-    name.stdout.on('data', function (data) {
-      nameString = data.toString().split("\n")[0].split(".ogg")[0];
-    });
-    name.stderr.on('data', function (data) {
-      console.log("error with name: "+data.toString());
-      callback("", "", false);
-    });
-    name.on('exit', function (code) {
-      if (code == 0) {
-        var hasher = require('crypto').createHash('sha1');
-        hasher.update(nameString+Date.now());
-        var hash = hasher.digest('hex');
-        var dl = child.spawn('youtube-dl', [
-            '--no-playlist', '--extract-audio', '--audio-format', 'vorbis', '--audio-quality', '128k',
-            obj.link, '-o', 'public/songs/'+obj.creator+'/'+nameString+hash+'.%(ext)s']);
-        dl.stdout.on('data', function (data) {
-          console.log(data.toString());
-        });
-        dl.stderr.on('data', function (data) {
-          console.log("error with downloading: "+data.tostring());
-          callback("", "", false);
-        });
-        dl.on('exit', function (code) {
-          callback(nameString, hash, true);
-        });
+    ytdl.getInfo(obj.link, function (err, info) {
+      if (err) {
+        console.log("There was an error getting info from the given link.");
+        callback('', '', false);
       }
+      nameString = info._filename.split('.webm')[0].split('-').slice(0, -1).join('-');
+
+      var hasher = require('crypto').createHash('sha1');
+      hasher.update(nameString+Date.now());
+      var hash = hasher.digest('hex');
+
+      ytdl.exec(obj.link,
+        ['--no-playlist', '--extract-audio', '--audio-format', 'vorbis',
+         '--audio-quality', '128k', '-o', 'public/songs/'+obj.creator+
+        '/'+nameString+hash+'.%(ext)s'], {}, function (err, output) {
+        if (err) {
+          console.log("There was an error downloading the song...");
+          callback('', '', false);
+        }
+        callback(nameString, hash, true);
+      });
     });
   }
 }
