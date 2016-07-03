91f2f429f3603a33de9541a1c888020e61ab70c6
Alex <alexrichards92@me.com>
2016-07-02 22:08:53 -0700

    ...

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@ window.onload = function () {
 
   /* Sends a message to the server to delete this user from their booth -- if
    * the user is the last one in the booth, then delete the booth too. */
-  window.onbeforeunload = function (event) {
+  window.onunload = function (event) {
     if (user) {
       socket.emit('deleteUser', {'booth':booth, 'user':user});
     }
